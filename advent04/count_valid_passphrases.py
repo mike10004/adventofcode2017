6a4a57a7759ca9aa4575a507cc90d2b36a023c64
Mike Chaberski <mchaberski@novetta.com>
2017-12-13 14:45:55 -0500

    day 4 part two

--- advent04/count_valid_passphrases.py
+++ advent04/count_valid_passphrases.py
@@ -3,17 +3,42 @@
 import re
 import sys
 
-def is_valid(passphrase):
-    tokens = re.split(r'\s+', passphrase.strip())
+def to_multiset(token): 
+    """ Creates a multiset from a string. A multiset is a set of tuples (c, n) where c is a character and n is a count of that character in the string."""
+    counts = {}
+    for ch in token:
+        try:
+            count = counts[ch]
+        except KeyError:
+            count = 0
+        counts[ch] = count + 1
+    multiset = set()
+    for ch in counts:
+        multiset.add((ch, counts[ch]))
+    return frozenset(multiset)
+
+def parse_tokens(passphrase):
+    return re.split(r'\s+', passphrase.strip())
+
+def is_tokens_not_anagrams(passphrase):
+    tokens = parse_tokens(passphrase)
+    multisets = [to_multiset(token) for token in tokens]
+    return len(multisets) == len(set(multisets))
+
+def is_tokens_unique(passphrase):
+    tokens = parse_tokens(passphrase)
     return len(tokens) == len(set(tokens))
 
 def main():
-    count = 0
+    tokens_unique_count = 0
+    tokens_not_anagrams_count = 0
     total = 0
     for passphrase in sys.stdin:
-        count += 1 if is_valid(passphrase) else 0
+        tokens_unique_count += 1 if is_tokens_unique(passphrase) else 0
+        tokens_not_anagrams_count += 1 if is_tokens_not_anagrams(passphrase) else 0
         total += 1
-    print("{} of {} passphrases are valid".format(count, total))
+    print("{} of {} passphrases have unique tokens".format(tokens_unique_count, total))
+    print("{} of {} passphrases have anagram-unique tokens".format(tokens_not_anagrams_count, total))
 
 if __name__ == '__main__':
     exit(main())
