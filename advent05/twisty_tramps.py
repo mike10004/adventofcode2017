2b30789cca8673565a48cdffb37c6e39142c5735
Mike Chaberski <mchaberski@novetta.com>
2017-12-14 16:17:13 -0500

    day 5: part two

--- advent05/twisty_tramps.py
+++ advent05/twisty_tramps.py
@@ -4,26 +4,43 @@
 
 import sys
 
-class JumpList:
+class JumpList1:
 
     def __init__(self, jumps):
         self.jumps = jumps
         self.cursor = 0
         self.steps = 0
-    
+
+    def _update(self):
+        self.jumps[self.cursor] += 1
+
     def advance(self):
         current = self.jumps[self.cursor]
-        self.jumps[self.cursor] += 1
+        self._update()
         self.cursor += current
         self.steps += 1
     
     def in_bounds(self):
         return self.cursor >= 0 and self.cursor < len(self.jumps)
 
+class JumpList2(JumpList1):
+
+    def _update(self):
+        offset = self.jumps[self.cursor]
+        if offset >= 3:
+            offset -= 1
+        else:
+            offset += 1
+        self.jumps[self.cursor] = offset
+
 def main():
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument("--part", choices=('one', 'two'), default='one')
+    args = parser.parse_args()
     text = sys.stdin.read()
     jumps = list(map(int, text.split()))
-    jumplist = JumpList(jumps)
+    jumplist = JumpList1(jumps) if args.part == 'one' else JumpList2(jumps)
     while jumplist.in_bounds():
         jumplist.advance()
     print("exited after {} jumps".format(jumplist.steps))
