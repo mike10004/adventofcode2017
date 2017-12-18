2f4b9fba97a323aced544bb4510884251b69ef85
Mike Chaberski <mchaberski@novetta.com>
2017-12-18 11:37:47 -0500

    day 7 part two

--- advent07/circus.js
+++ advent07/circus.js
@@ -7,6 +7,7 @@ class Node {
         this.name = name;
         this.weight = weight;
         this.children = children;
+        this.load = 0; // load is sum of weights of all descendents
     }
 
     equals(other) {
@@ -14,8 +15,37 @@ class Node {
     }
 
     toString() {
-        return JSON.stringify(this);
+        const d = this.depth();
+        return this.name + '(depth=' + d + ', freight=' + this.freight + ')';
     }
+
+    depth() {
+        let node = this;
+        let d = 0;
+        while (node.parent) {
+            node = node.parent;
+            d++;
+        }
+        return d;
+    }
+    
+    parentToRootPath() { // excludes this node
+        let node = this;
+        const path = [];
+        while (node.parent) {
+            path.push(node.parent);
+            node = node.parent;
+        }
+        return path;
+    }
+
+    siblings() {
+        if (!this.parent) {
+            return [];
+        }
+        return this.parent.children.filter(n => n !== this);
+    }
+
 }
 
 function parseNode(line) {
@@ -40,6 +70,63 @@ function parseNode(line) {
     {line: 'tknk (41) -> ugml, padx, fwft', expected: new Node('tknk', 41, ['ugml', 'padx', 'fwft'])},
 ]);
 
+function Queue() {
+    const items = [];
+    const self = this;
+    
+    this.push = function(item) {
+        items.push(item);
+    }
+
+    this.pop = function() {
+        return items.splice(0, 1)[0];
+    }
+
+    this.isEmpty = function() {
+        return items.length === 0;
+    }
+}
+
+function breadthFirstTraversal(root, callback) {
+    const queue = new Queue();
+    queue.push(root);
+    let lastDepth;
+    while (!queue.isEmpty()) {
+        const node = queue.pop();
+        const depth = node.depth();
+        callback(node, depth, lastDepth);
+        lastDepth = depth;
+        node.children.forEach(queue.push);
+    }
+}
+
+class Multimap {
+
+    constructor() {
+        this.data = new Map();
+    }
+
+    put(key, value) {
+        this.get(key).push(value);
+    }
+
+    get(key) {
+        if (!this.data.has(key)) {
+            this.data.set(key, []);
+        }
+        return this.data.get(key);
+    }
+
+    has(key) {
+        return this.data.has(key);
+    }
+
+    keys() {
+        return Array.from(this.data.keys());
+    }
+
+}
+
 (function(inputFile){
     if (!inputFile) {
         console.error("must specify input file as argument");
@@ -47,18 +134,60 @@ function parseNode(line) {
     }
     const text = fs.readFileSync(inputFile, {encoding: 'utf-8'});
     const lines = text.split('\n');
-    const nodes = {};
+    const nodes = new Map();
     lines.forEach(line => {
         if (line.trim()) { // skip empty
             const node = parseNode(line);
-            nodes[node.name] = node;
+            nodes.set(node.name, node);
         }
     });
-    Object.values(nodes).forEach(node => {
+    console.log(nodes.size, "nodes");
+    for (let node of nodes.values()) {
         node.children.forEach(childName => {
-            nodes[childName].parent = nodes[node.name];
+            nodes.get(childName).parent = nodes.get(node.name);
         });
+        node.children = node.children.map(name => nodes.get(name));
+    }    
+    const roots = Array.from(nodes.values()).filter(node => !node.parent);
+    assert.equal(roots.length, 1, "expect exactly 1 root node");
+    const root = roots[0];
+    console.log("root", root.toString());
+    for (let node of nodes.values()) {
+        node.parentToRootPath().forEach(p => {
+            p.load += node.weight;
+        });
+    }
+    for (let node of nodes.values()) {
+        node.freight = node.weight + node.load;
+    };
+    const nodesByDepth = new Multimap();
+    breadthFirstTraversal(root, (node, depth, lastDepth) => {
+        if (depth !== lastDepth) {
+            process.stdout.write('\n');
+        }
+        nodesByDepth.put(depth, node);
+        process.stdout.write(node.toString() + ' ');
     });
-    Object.values(nodes).filter(node => !node.parent)
-        .forEach(node => console.log("parentless node", node.toString()));
+    process.stdout.write('\n\n');
+    const nodesSortedByDepth = Array.from(nodes.values());
+    nodesSortedByDepth.sort((a, b) => {
+        return a.depth() - b.depth();
+    });
+    nodesSortedByDepth.reverse();
+    let foundUnique = false;
+    for (let node of nodesSortedByDepth) {
+        const siblingFreights = new Set();
+        node.siblings().forEach(sibling => siblingFreights.add(sibling.freight));
+        if (siblingFreights.size === 1) {
+            const siblingFreight = siblingFreights.values().next().value;
+            if (node.freight !== siblingFreight) {
+                console.log("highest node with unique freight among siblings", node.toString(), "should be", siblingFreight);
+                foundUnique = true;
+                break;
+            }
+        }
+    }
+    if (!foundUnique) {
+        console.log("no node with unique freight found");
+    }
 })(args[0]);
