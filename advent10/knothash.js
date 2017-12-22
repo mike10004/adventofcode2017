291255d74219db05f6e4fca1765123a35facb21e
Mike Chaberski <mchaberski@novetta.com>
2017-12-22 13:48:23 -0500

    basic tests work for array-based implementation

--- advent10/knothash.js
+++ advent10/knothash.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
+const assert = require('assert');
+const util = require('util');
 
 class CircularList {
 
-    constructor() {
-        this.values = [];
+    constructor(values) {
+        this.values = values || [];
     }
 
     add(value) {
@@ -14,12 +16,39 @@ class CircularList {
         return this.values.length;
     }
 
+    mod(index) {
+        return ((index % this.values.length) + this.values.length) % this.values.length;
+    }
+
     get(index) {
-        return this.values[index];
+        return this.values[this.mod(index)];
     }
 
     toArray() {
         return this.values.concat([]);
+    }
+
+    slice(pos, length) {
+        const s = [];
+        for (let i = pos; i < (pos + length); i++) {
+            s.push(this.get(i));
+        }
+        return s;
+    }
+
+    set(index, value) {
+        this.values[this.mod(index)] = value;
+    }
+
+    reverse(pos, length) {
+        assert(length <= this.size(), util.format("length %s > size %s", length, this.size()));
+        for (let i = 0; i < parseInt(length / 2); i++) {
+            const k = pos + i;
+            const z = pos + length - (i + 1);
+            const kv = this.get(k), zv = this.get(z);
+            this.set(z, kv);
+            this.set(k, zv);
+        }
     }
 }
 
--- advent10/test.js
+++ advent10/test.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 const {CircularList} = require('./knothash');
+const util = require('util');
 
 (function(){
     function testListFromArray(array) {
@@ -12,8 +13,8 @@ const {CircularList} = require('./knothash');
             assert.equal(actual, value, 'get(' + index + '): expected ' + value + ' but got ' + actual);
         })
         assert.deepStrictEqual(list.toArray(), array, 'toArray');
-        assert(typeof list.get(array.length), 'undefined', 'get(index > length) is undefined');
-        assert(typeof list.get(-1), 'undefined', 'get(-1) undefined');
+        assert.equal(list.get(array.length), array[0], 'get(array.length) == get(0)');
+        assert.equal(list.get(-1), array[array.length - 1], 'get(-1) === last element');
     }
 
     [
@@ -23,6 +24,47 @@ const {CircularList} = require('./knothash');
         ['a', 'b', 'c'],
         ['a', 'b', 'c', 'd']
     ].forEach(array => testListFromArray(array));
+
+    const three = new CircularList([1, 2, 3]);
+    assert.deepEqual(three.mod(0), 0);
+    assert.deepEqual(three.mod(1), 1);
+    assert.deepEqual(three.mod(2), 2);
+    assert.deepEqual(three.mod(3), 0);
+    assert.deepEqual(three.mod(4), 1);
+    assert.deepEqual(three.mod(-1), 2);
+    assert.deepEqual(three.mod(-2), 1);
+    assert.deepEqual(three.mod(-3), 0);
+    assert.deepEqual(three.mod(-4), 2);
+    assert.deepEqual(three.mod(-5), 1);
+
+    function ReverseTestCase(values, pos, length, expected) {
+        this.values = values;
+        this.pos = pos;
+        this.length = length;
+        this.expected = expected;
+        const me = this;
+
+        this.toString = function() {
+            return JSON.stringify(me);
+        }
+    }
+
+    function testReverse(testCase) {
+        const list = new CircularList(testCase.values);
+        list.reverse(testCase.pos, testCase.length);
+        const actual = list.toArray();
+        assert.deepEqual(actual, testCase.expected, util.format("[%s].reverse(%s, %s) produced [%s] instead of [%s]", testCase.values, testCase.pos, testCase.length, actual, testCase.expected));
+    }
+
+    [
+        new ReverseTestCase([], 0, 0, []),
+        new ReverseTestCase([1], 0, 1, [1]),
+        new ReverseTestCase([1, 2], 0, 2, [2, 1]),
+        new ReverseTestCase([1, 2, 3], 1, 1, [1, 2, 3]),
+        new ReverseTestCase([1, 2, 3, 4], 1, 2, [1, 3, 2, 4]),
+        new ReverseTestCase([1, 2, 3, 4], 2, 3, [3, 2, 1, 4]),
+        new ReverseTestCase([0, 1, 2, 3, 4, 5], 5, 3, [0, 5, 2, 3, 4, 1]),
+    ].forEach(testCase => testReverse(testCase));
 })();
 
 console.log('tests passed');
