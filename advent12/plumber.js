640b7be2a7074574e0aa91c90d157297e3a049f1
Mike Chaberski <mchaberski@novetta.com>
2017-12-27 16:16:32 -0500

    day 12 part one

---
+++ advent12/plumber.js
@@ -0,0 +1,71 @@
+const structures = require('../common/structures');
+const util = require('util');
+const assert = require('assert');
+
+function makeGraphFromMultimap(mm) {
+    const g = new structures.UndirectedGraph();
+    mm.keys().forEach(key => {
+        mm.get(key).forEach(value => {
+            g.addEdge(key, value);
+        });        
+    });
+    return g;
+}
+
+function parseMultimap(text) {
+    const r = /(\d+) <-> ((?:\d+)(, (\d+))*)/g;
+    const mm = new structures.Multimap();
+    let m;
+    while ((m = r.exec(text)) !== null) {
+        const key = parseInt(m[1], 10);
+        let values = m[2].split(/,\s*/);
+        values = values.map(token => parseInt(token, 10));
+        mm.putAll(key, values);
+    }
+    return mm;
+}
+
+(function test_parseMultimap(testCases){
+    let mm = parseMultimap("3 <-> 1465, 1568\n" +
+        "4 <-> 359, 1047, 1215, 1580, 1969\n" +
+        "5 <-> 613\n" +
+        "6 <-> 49, 617, 1213");
+    assert.equal(mm.keyCount(), 4);
+    assert.equal(mm.valueCount(), 11);
+})(
+    
+);
+
+function findReachable(g, vertex) {
+    const reachable = new Set();
+    let visitedCount = 0;
+    g.traverseDepthFirst(0, p => {
+        reachable.add(p);
+        visitedCount++;
+    });
+    return reachable;
+}
+
+(function test() {
+    const mm = new structures.Multimap([
+        [0, [2]],
+        [1, [1]],
+        [2, [0, 3, 4]],
+        [3, [2, 4]],
+        [4, [2, 3, 6]],
+        [5, [6]],
+        [6, [4, 5]],
+    ]);
+    const g = makeGraphFromMultimap(mm);
+    const reachableFrom0 = findReachable(g, 0);
+    console.log(util.format("%d programs reachable from %s: %s", reachableFrom0.size, 0, Array.from(reachableFrom0)));
+    assert.equal(6, reachableFrom0.size);
+})();
+
+const args = process.argv.slice(2);
+if (args.length > 0) {
+    const inputText = require('fs').readFileSync(args[0], 'utf8');
+    const g = makeGraphFromMultimap(parseMultimap(inputText));
+    const reachable = findReachable(g, 0);
+    console.log(util.format("%d programs reachable from %d", reachable.size, 0));
+}
--- common/structures.js
+++ common/structures.js
@@ -75,12 +75,26 @@ function breadthFirstTreeTraversal(root, callback) {
 
 class Multimap {
 
-    constructor() {
+    constructor(entries) {
         this.data = new Map();
+        this._numValues = 0;
+        if (entries) {
+            entries.forEach(entry => {
+                this.data.set(entry[0], entry[1]);
+                this._numValues += entry[1].length;
+            });
+        }
+    }
+
+    putAll(key, valueArray) {
+        valueArray.forEach(value => {
+            this.put(key, value);
+        });
     }
 
     put(key, value) {
         this.get(key).push(value);
+        this._numValues++;
     }
 
     get(key) {
@@ -98,6 +112,13 @@ class Multimap {
         return Array.from(this.data.keys());
     }
 
+    keyCount() {
+        return this.data.size;
+    }
+
+    valueCount() {
+        return this._numValues;
+    }
 }
 
 class QSet {
@@ -126,11 +147,141 @@ class QSet {
     }
 }
 
+class Labeled {
+    constructor(label) {
+        this.label = label;
+    }
+}
+
+class Vertex extends Labeled {
+    constructor(label) {
+        super(label);
+    }
+}
+
+class Table {
+
+    constructor() {
+        this.rows = new Map();
+        this.rowLabels = new Set();
+        this.columnLabels = new Set();
+        this.allLabels = new Set();
+    }
+
+    put(row, col, value) {
+        this.rowLabels.add(row);
+        this.columnLabels.add(col);
+        this.allLabels.add(row);
+        this.allLabels.add(col);
+        let colMap = this.rows.get(row);
+        if (!colMap) {
+            colMap = new Map();
+            this.rows.set(row, colMap);
+        }
+        colMap.set(col, value);
+    }
+
+    get(row, col) {
+        const colMap = this.rows.get(row);
+        if (colMap) {
+            return colMap.get(col);
+        }
+    }
+
+    columnMap(row) {
+        return new Map(this.rows.get(row) || new Map());
+    }
+
+    rowMap(col) {
+        const m = new Map();
+        for (let e of this.rows) {
+            const row = e[0], colMap = e[1];
+            if (colMap.has(col)) {
+                m.set(row, colMap.get(col));
+            }
+        }
+        return m;
+    }
+}
+
+function getSorter(value) {
+    if (typeof value === 'number') {
+        return (a, b) => a - b;
+    } else if (typeof value === 'string') {
+        return (a, b) => (a < b) ? -1 : (a === b ? 0 : 1); // String.prototype.localeCompare.call
+    }
+}
+
+class SymmetricTable extends Table {
+    
+    constructor() {
+        super();
+    }
+
+    normalize(row, col) {
+        const coordinates = [row, col];
+        coordinates.sort(getSorter(row));
+        return coordinates;
+    }
+
+    put(row, col, value) {
+        const coordinates = this.normalize(row, col);
+        return super.put(coordinates[0], coordinates[1], value);
+    }
+
+    get(row, col) {
+        const coordinates = this.normalize(row, col);
+        return super.get(coordinates[0], coordinates[1]);
+    }
+}
+
+class UndirectedGraph {
+
+    constructor() {
+        this.matrix = new SymmetricTable();
+    }
+
+    normalizeEdgeLabel(label) {
+        return typeof label === 'undefined' ? true : label;
+    }
+
+    addEdge(v1, v2, label) {
+        this.matrix.put(v1, v2, this.normalizeEdgeLabel(label));
+    }
+
+    adjacent(v) {
+        const rowMap = this.matrix.rowMap(v), colMap = this.matrix.columnMap(v);
+        const rowKeys = Array.from(rowMap.keys());
+        const colKeys = Array.from(colMap.keys());
+        const all = rowKeys.concat(colKeys);
+        return all;
+    }
+
+    traverseDepthFirst(root, callback) {
+        const visited = new Set();
+        const g = this;
+        const doTraverse = function(v) {
+            if (!visited.has(v)) {
+                visited.add(v);
+                callback(v);
+                const neighbors = g.adjacent(v);
+                neighbors.forEach(neighbor => {
+                    doTraverse(neighbor, callback);
+                });
+            }
+        };
+        doTraverse(root);
+    }
+}
+
 module.exports = {
     Multimap: Multimap,
     Queue: Queue,
     TreeNode: TreeNode,
     breadthFirstTreeTraversal: breadthFirstTreeTraversal,
-    QSet: QSet
+    QSet: QSet,
+    UndirectedGraph: UndirectedGraph,
+    Table: Table,
+    SymmetricTable: SymmetricTable
 };
 
